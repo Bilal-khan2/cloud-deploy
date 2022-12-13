28fba1f46f848dff3122ba8da4294d80216cd2e1
Bilal-khan2 <bilalhamidkhan.20@gmail.com>
2022-12-14 01:55:53 +0500

    commit2

--- server.mjs
+++ server.mjs
@@ -1,16 +1,17 @@
-console.log("server file here")
-
 import express from 'express';
+import path from 'path';
+console.log("server file here")
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
+app.get('/abc', (req, res) => {
 
     console.log("request ip", req.ip)
-   
-
   res.send('Hello World!' + new Date().toString())
+
 })
+const __dirname = path.resolve();
+app.use('/', express.static(path.join(__dirname,'./web/build')))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
